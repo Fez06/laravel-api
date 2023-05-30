b66b0212f248e8257e368b099a8377e04d2695a5
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-05-30 18:16:40 +0200

    problemi con la creazione della foreign key

---
+++ database/migrations/2023_05_30_151821_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('author', 150)->nullable();
+            $table->text('content');
+            $table->integer('post_id');
+            $table->timestamps();
+        });
+
+        Schema::table('comments', function($table) {
+            $table->foreign('post_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
